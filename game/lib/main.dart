4636d6379e0dc27d1f888498e62b727ae8b3a9d7
MahaIhsham <49373434+MahaIhsham@users.noreply.github.com>
2022-03-22 01:15:26 +0500

    Add files via upload

---
+++ game/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:game/home_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+     return new MaterialApp(
+      theme: new ThemeData(primaryColor: Colors.black),
+      home: new HomePage(),
+    );
+  }
+}
+
+
